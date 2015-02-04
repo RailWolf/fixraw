8d3c2bcc8d6ebd71c9ddf2173e776e4d4decae56
RailWolf <wailrofl@gmail.com>
2015-02-03 23:38:31 -0800

    updates

--- fixraw.rb
+++ fixraw.rb
@@ -5,20 +5,16 @@
 # wailrofl@gmail.com
 #
 # Syntax: fixraw.rb <filename>
-# FixRaw will correctly set offsets 4 through 7 on RAW file. Currently only supports Nikon. 
+# FixRaw will correctly set offsets 4 through 7 on a RAW file. Currently only supports Nikon. 
 #
 
 file = ARGV[0]
 hexvalue = [0x00]
-offset = 4 
-  
-# Loop binwrite until offset 4 through 7 have been written  
-until offset == 8 do 
- if offset == 7 then hexvalue = [0x08] end
-  IO.binwrite(file, hexvalue.pack("C"), offset)
-  offset += 1
-end
 
+4.upto(7) do |offset|
+  if offset == 7 then hexvalue = [0x08] end
+    IO.binwrite(file, hexvalue.pack("C"), offset)
+    end
 
 
 
